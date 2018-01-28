9b56aa59827f1efb733d4cd2e48810eb07c47213
jlstevens <jlstevens@ed.ac.uk>
2018-01-28 23:06:11 +0000

    Deleted trailing whitespace

--- parambokeh/__init__.py
+++ parambokeh/__init__.py
@@ -57,7 +57,7 @@ class Widgets(param.ParameterizedFunction):
 
     button_text = param.String(default="Run", doc="""
         Text to show on the 'next_n'/run button.""")
-    
+
     show_labels = param.Boolean(default=True)
 
     display_threshold = param.Number(default=0,precedence=-10,doc="""
@@ -375,7 +375,7 @@ class Widgets(param.ParameterizedFunction):
             widgets += [self.widget(pname) for pname in ordered_params]
 
         if self.p.button and not (self.p.callback is None and self.p.next_n==0):
-            display_button = Button(label=self.p.button_text)            
+            display_button = Button(label=self.p.button_text)
             def click_cb():
                 # Execute and clear changes since last button press
                 try:
