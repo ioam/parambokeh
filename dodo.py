2823fa150d49068e9303bfdacab0a0f4efc1e54c
C. E. Ball <ceball@gmail.com>
2018-03-01 10:15:47 +0000

    Switch to expressing dependencies in setup.py.

--- dodo.py
+++ dodo.py
@@ -1,38 +1,18 @@
-DOIT_CONFIG = {'verbosity': 2}
-
-from ioamdoit import *
+from pyct import *
 
 # The aim would be to not have anything much here, but right now
 # that's not easy because of awkward installation/specification of
 # dependencies across projects.
 
-def task_install_pip_dependencies():
-    return {'actions': ['pip install pytest-nbsmoke']}
-
-def task_install_doc_dependencies():
-    # would not need to exist if nbsite had conda package
-    return {
-        'actions': [
-            'conda install -y -q -c conda-forge notebook ipython sphinx beautifulsoup4 graphviz selenium phantomjs',
-            'pip install nbsite sphinx_ioam_theme']
-        }
-
-def task_lint():
-    return {'actions': ['flake8 --ignore E,W parambokeh']}
-
-
-def task_lint_nb():
-    return {'actions': ['pytest --nbsmoke-lint examples/']}
-
-
-def task_test_nb():
-    return {'actions': ['pytest --nbsmoke-run examples/']}
-
 
 def task_all_tests():
     return {'actions': [],
-            'task_dep': ['lint','lint_nb','test_nb']}
+            'task_dep': ['lint','nb_lint','nb_tests']}
 
+# TODO: move command to pyct and read deps from setup.py extras_require['docs']
+def task_install_doc_dependencies():
+    return {
+        'actions': ['conda install -y -c pyviz/label/dev -c conda-forge nbsite sphinx_ioam_theme']}
 
 def task_docs():
     # TODO: could do better than this, or nbsite could itself use doit
--- setup.py
+++ setup.py
@@ -1,35 +1,49 @@
 #!/usr/bin/env python
+
 import os
 import sys
 
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+from setuptools import setup
 
-setup_args = {}
-install_requires = ['param>=1.5.1', 'bokeh>=0.12.10']
-
-setup_args.update(dict(
+setup_args = dict(
     name='parambokeh',
-    version="0.2.1",
-    install_requires = install_requires,
-    url = 'https://github.com/ioam/parambokeh',
     description='ParamBokeh provides an easy way to generate a UI for param based classes in the notebook or on bokeh server.',
+    version=os.environ.get("VERSIONHACK","0.2.1"),
+    url = 'https://ioam.github.io/parambokeh/',
     long_description=open('README.rst').read() if os.path.isfile('README.rst') else 'Consult README.rst',
-    author= "IOAM",
-    author_email= "holoviews@gmail.com",
-    maintainer= "IOAM",
-    maintainer_email= "holoviews@gmail.com",
+    author= "pyviz contributors",
+    author_email= "dev@pyviz.org",
+    license = "BSD 3-Clause",
     platforms=['Windows', 'Mac OS X', 'Linux'],
     packages = ["parambokeh"],
     provides = ["parambokeh"],
-))
+
+    python_requires = ">=2.7",
+
+    install_requires = [
+        'param >=1.5.1',
+        'bokeh >=0.12.10'
+    ],
+
+    tests_require = [
+        'holoviews >=1.9.0',
+        'pandas',
+        'notebook',
+        'flake8',
+        'pyparsing',
+        'pytest',
+        'nbsmoke',
+    ],
+
+    extras_require = {
+        'docs': [
+            'nbsite',
+            'sphinx_ioam_theme'
+        ]
+    }
+
+)
 
 
 if __name__=="__main__":
-    if ('upload' in sys.argv) or ('sdist' in sys.argv):
-        import parambokeh
-        parambokeh.__version__.verify(setup_args['version'])
-
     setup(**setup_args)
